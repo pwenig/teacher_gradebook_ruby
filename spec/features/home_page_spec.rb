2e2e4e6e89504b63c4c425614680c4e352c81789
Paul Wenig <pwenig@gmail.com>
2016-03-12 07:55:51 -0700

    Initial spec, basic design

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'spec_helper'
+
+feature 'home page' do
+
+  scenario 'teacher visits home page' do
+    visit root_path
+    expect(page).to have_css "a", text: "Grade Book"
+
+  end
+end
