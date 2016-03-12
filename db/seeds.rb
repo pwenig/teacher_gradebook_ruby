217a076f85792f6a4c871069de81adeecb90593c
Paul Wenig <pwenig@gmail.com>
2016-03-12 15:04:26 -0700

    Updated seeds with status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Student.create(name: 'Jeff Spicoli', grade: 50)
-Student.create(name: 'Mike Damone', grade: 70)
-Student.create(name: 'Linda Barrett', grade: 90)
+Student.create(name: 'Jeff Spicoli', grade: 50, status: "red")
+Student.create(name: 'Mike Damone', grade: 70, status: "none")
+Student.create(name: 'Linda Barrett', grade: 90, status: "none")
